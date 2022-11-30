e54caad83629e2033a07ab4bdf9fcfaff5f9f9b0
ThanhTam <thanhtam.v2t@gmail.com>
2022-11-30 16:49:22 +0700

    fix: refresh build

--- cjs/types/crypto/config.js
+++ cjs/types/crypto/config.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.slip44 = void 0;
+exports.setSlip44 = exports.slip44 = void 0;
 exports.slip44 = {
     coinType: 118,
 };
+function setSlip44(value) {
+    exports.slip44.coinType = value;
+}
+exports.setSlip44 = setSlip44;
--- esm/types/crypto/config.js
+++ esm/types/crypto/config.js
@@ -1,3 +1,6 @@
 export const slip44 = {
     coinType: 118,
 };
+export function setSlip44(value) {
+    slip44.coinType = value;
+}
